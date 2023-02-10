604e060c4abedb193786abf6d06f7bb29a5350df
Fahim <ishtiaq.fahim@northsouth.edu>
2023-02-10 23:51:05 +0600

    up you go

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
 const app = new Application();
+const router=new Router();
+
+router.get("/:path", (ctx) => {
+    ctx.response.headers.append("Content-Type", "text/html");
+    if(ctx.params.path=="gmail"){
+        ctx.response.body = `GooglePass`;
+    }
+});
+
+
+router.get("/:path1/:path2", (ctx) => {
+    ctx.response.headers.append("Content-Type", "text/html");
+    ctx.response.body = `${ctx.params.path1} ${ctx.params.path2}`;
+});
+
+
+
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
